6e18ae54ef6ab6bb62c4408ed991f21258ffb1f1
André Severino <andre@andrewd.com.br>
2015-10-13 18:52:05 -0300

    Arquivos do site

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    
+    var link_demonstracao_cliente   = 'http://lab.andrewd.com.br/cliente/';
+    var link_demonstracao_atendente = 'http://lab.andrewd.com.br/atendente/';
+    
+    $("a[rel='tipsy']").tipsy({gravity: 'n'});
+
+    $(document).bind("contextmenu",function(e){
+        return false;
+    });
+    
+    $(".fancybox-effects-d").fancybox({
+        openEffect : 'elastic',
+        openSpeed  : 150,
+
+        closeEffect : 'elastic',
+        closeSpeed  : 450,
+
+        closeClick : true,
+
+        helpers : {
+            overlay : null
+        }
+    });
+    
+    $('#demonstracao-atendimento').click(function(){
+
+        var width   = 500;
+        var height  = 540;
+        var left    = (screen.width - width) / 2;
+        var top     = ((screen.height - height) / 2) - 30;
+
+        window.open(link_demonstracao_cliente, 'at',  'height=' +height+ ', width=' +width+ ',status=no, toolbar=no, resizable=0, scrollbars=no, minimizable=no, left=' +left+ ', top=' +top);
+
+    });
+    
+    var altura_div_versao = $('#list_version').height();
+    
+    if( altura_div_versao > 400 )
+        $('#list_version').css('height', '400px');
+    
+    $('#ver-todas-atualizacoes').click(function(){
+        $("#list_version").animate(
+            {"height": altura_div_versao+"px"},
+        "fast");
+        $(this).hide("slow");
+    });
+    
+});
